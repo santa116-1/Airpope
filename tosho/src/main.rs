f33bc7c6a2465e5b3af0a6877fbd9f8639968bbc
santa116-1 <totoplife116@gmail.com>
2024-02-17 15:54:54 +0700

    docs(cli): add rustdoc for main

--- tosho/src/main.rs
+++ tosho/src/main.rs
@@ -1,3 +1,42 @@
+//! # tosho
+//!
+//! [`tosho-mango`](https://github.com/noaione/tosho-mango) (or `tosho`) is a downloader but can also
+//! be considered an almost full-blown replacement for the app/web version, with the exception of
+//! currency purchase, as a simple CLI application.
+//!
+//! Currently we support the following source:
+//! - [MU! by SQ](https://crates.io/crates/tosho-musq)
+//! - [KM by KC](https://crates.io/crates/tosho-kmkc)
+//! - [AM by AP](https://crates.io/crates/tosho-amap)
+//! - [SJ/M by V](https://crates.io/crates/tosho-sjv)
+//!
+//! ## Installation
+//!
+//! You can install by cloning the repository then building manually...
+//!
+//! Or...
+//!
+//! ```bash
+//! $ cargo install tosho
+//! ```
+//!
+//! Or, if you have [cargo-binstall](https://github.com/cargo-bins/cargo-binstall)...
+//!
+//! ```bash
+//! $ cargo binstall tosho
+//! ```
+//!
+//! ## Usage
+//!
+//! Refer to the [repo](https://github.com/noaione/tosho-mango) on how to authenticate with each source.<br />
+//! For a list of available commands, use the `--help` argument.
+//!
+//! [![asciicast](https://asciinema.org/a/636303.svg)](https://asciinema.org/a/636303)
+//!
+//! ## License
+//!
+//! This project is licensed with MIT License ([LICENSE](https://github.com/noaione/tosho-mango/blob/master/LICENSE) or <http://opensource.org/licenses/MIT>)
+
 use std::path::PathBuf;
 
 use clap::Parser;
